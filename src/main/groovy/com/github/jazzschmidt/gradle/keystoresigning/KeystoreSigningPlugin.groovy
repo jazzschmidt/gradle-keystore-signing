38c2e701b343cbd307db8e3de8e67044ef5192bd
Carsten Schmidt <jazzschmidt@icloud.com>
2017-05-11 22:33:58 +0200

    Adds an empty plugin

---
+++ src/main/groovy/com/github/jazzschmidt/gradle/keystoresigning/KeystoreSigningPlugin.groovy
@@ -0,0 +1,12 @@
+package com.github.jazzschmidt.gradle.keystoresigning
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class KeystoreSigningPlugin implements Plugin<Project> {
+
+    @Override
+    void apply(Project project) {
+
+    }
+}
