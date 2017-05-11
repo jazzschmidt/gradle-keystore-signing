9901b89dd6bc5e9ae4d9a7167de177302767560f
Carsten Schmidt <jazzschmidt@icloud.com>
2017-05-12 00:04:42 +0200

    Simple test

---
+++ src/test/groovy/com/github/jazzschmidt/gradle/keystoresigning/KeystoreSigningPluginSpec.groovy
@@ -0,0 +1,18 @@
+package com.github.jazzschmidt.gradle.keystoresigning
+
+import org.gradle.api.Project
+import org.gradle.testfixtures.ProjectBuilder
+import spock.lang.Specification
+
+class KeystoreSigningPluginSpec extends Specification {
+
+    def 'plugin adds a signing task to the project'() {
+        when:
+        Project project = ProjectBuilder.builder().build()
+        project.pluginManager.apply(KeystoreSigningPlugin)
+
+        then:
+        project.tasks.findByName('signing') != null
+    }
+
+}
