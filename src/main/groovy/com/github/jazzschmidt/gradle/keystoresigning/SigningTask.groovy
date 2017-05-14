8ce53d888f8077bd267cc1c758ddc333604eb355
Carsten Schmidt <jazzschmidt@icloud.com>
2017-05-14 23:15:30 +0200

    Declare basic task input and output parameters

--- src/main/groovy/com/github/jazzschmidt/gradle/keystoresigning/SigningTask.groovy
+++ src/main/groovy/com/github/jazzschmidt/gradle/keystoresigning/SigningTask.groovy
@@ -1,6 +1,20 @@
 package com.github.jazzschmidt.gradle.keystoresigning
 
 import org.gradle.api.DefaultTask
+import org.gradle.api.tasks.InputFiles
+import org.gradle.api.tasks.OutputFiles
+import org.gradle.api.tasks.TaskAction
 
 class SigningTask extends DefaultTask {
+
+    @InputFiles
+    Set<File> archives
+
+    @OutputFiles
+    Set<File> signedArchives
+
+    @TaskAction
+    void signArchives() {
+        logger.info('Signing archives...')
+    }
 }
