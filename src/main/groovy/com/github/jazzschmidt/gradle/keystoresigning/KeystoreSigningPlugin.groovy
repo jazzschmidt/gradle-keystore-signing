5ec0ac9f8378301c412a0ff341320720fc26cfae
Carsten Schmidt <jazzschmidt@icloud.com>
2017-05-17 23:11:23 +0200

    Extract initialisation of configuration extension and extract strings as constants

--- src/main/groovy/com/github/jazzschmidt/gradle/keystoresigning/KeystoreSigningPlugin.groovy
+++ src/main/groovy/com/github/jazzschmidt/gradle/keystoresigning/KeystoreSigningPlugin.groovy
@@ -5,20 +5,30 @@ import org.gradle.api.Project
 
 class KeystoreSigningPlugin implements Plugin<Project> {
 
-    private class Configuration implements KeystoreSigningConfiguration { }
+    final public static String ARTIFACT_CONFIGURATION = 'signedArchives'
+
+    final private class Extension implements KeystoreSigningConfiguration {
+        final public static String EXTENSION_NAME = 'signing'
+    }
 
     @Override
     void apply(Project project) {
         def task = project.tasks.create('signArchives', SigningTask)
 
-        project.configurations.create('signedArchives')
-        project.extensions.add('signing', new Configuration())
+        project.configurations.create(ARTIFACT_CONFIGURATION)
 
-        def extension = project.extensions.getByName('signing')
+        def extension = createExtensionFromProperties(project)
+        project.extensions.add(Extension.EXTENSION_NAME, extension)
+    }
+
+    private Extension createExtensionFromProperties(Project project) {
+        def extension = new Extension()
 
         def keystore = project.findProperty('keystoresigning.keystore')
         extension.keystore = keystore ? project.file(keystore) : null
         extension.alias = project.findProperty('keystoresigning.alias')
         extension.password = project.findProperty('keystoresigning.password')
+
+        return extension
     }
 }
